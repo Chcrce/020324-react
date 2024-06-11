0ba9aaeedc5fac83d5aea858608fd23c95575b8b
Chcrce <ibrahimucak9@gmail.com>
2024-06-11 11:31:27 +0300

    Create abc.js

---
+++ src/components/abc.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+// reactı tanımladı ve bir component arrayi ekledi 
+
+class Counter extends Component {
+    //counter sınıfını componente
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleClick}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
